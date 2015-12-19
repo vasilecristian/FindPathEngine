11e39c1f03145c64235aecc462f5ea6832b86f3b
vasilecristian <vasile.cristian@gmail.com>
2015-12-19 17:54:44 +0200

    - add a namespace.

--- include/FindPathEngine/FindPathEngine.h
+++ include/FindPathEngine/FindPathEngine.h
@@ -1,4 +1,5 @@
-
+#ifndef FINDPATHENGINE_H
+#define FINDPATHENGINE_H
 
 #include <vector>
 #include <memory>
@@ -7,62 +8,140 @@
 #include <iostream>
 #include <functional>
 
-/** This is the interface that must be implemented by user.*/
-class NavMeshBase
+namespace fpe
 {
-public:
-	/** Must be implemented in derived class! 
-	* This function will calculate the distance(aka the cost) to the goal.
-	* Is the Heuristic, that will return the distance from nodeIndex to goalIndex.
-	* @param goalIndex is the index of the node that represent the target.
-	* @param nodeIndex is the index of the node that you want to calculate the distance.
-	* @return the estimated distance from nodeIndex to goalIndex.*/
-	virtual int ComputeGoalDistanceEstimate(int goalIndex, int nodeIndex) = 0;
+	/** This is the interface that must be implemented by user.*/
+	class NavMeshBase
+	{
+	public:
+		/** Must be implemented in derived class!
+		* This function will calculate the distance(aka the cost) to the goal.
+		* Is the Heuristic, that will return the distance from nodeIndex to goalIndex.
+		* @param goalIndex is the index of the node that represent the target.
+		* @param nodeIndex is the index of the node that you want to calculate the distance.
+		* @return the estimated distance from nodeIndex to goalIndex.*/
+		virtual int ComputeGoalDistanceEstimate(int goalIndex, int nodeIndex) = 0;
 
-	/**  Must be implemented in derived class! 
-	* This function will calculate the movement cost from a node to a neighbor node.
-	* @param nodeIndex is the node index .
-	* @param neighborIndex is the node index .
-	* @return the cost to move from node to neighbor.*/
-	virtual int ComputeCost(int nodeIndex, int neighborIndex) = 0;
-	
-	/** Must be implemented in derived class! 
-	* Use this to get all the valid heighbod nodes. A valid node
-	* is considered a node that can be used in the path => do not return the
-	* nodes which have collision. 
-	* @param nodeIndex is the node that you want to get the neighbors for.
-	* @return a list with neighbor nodes Indexes.*/
-	virtual std::vector<int> GetNeighbors(int nodeIndex) = 0;
-};
+		/**  Must be implemented in derived class!
+		* This function will calculate the movement cost from a node to a neighbor node.
+		* @param nodeIndex is the node index .
+		* @param neighborIndex is the node index .
+		* @return the cost to move from node to neighbor.*/
+		virtual int ComputeCost(int nodeIndex, int neighborIndex) = 0;
 
+		/** Must be implemented in derived class!
+		* Use this to get all the valid heighbod nodes. A valid node
+		* is considered a node that can be used in the path => do not return the
+		* nodes which have collision.
+		* @param nodeIndex is the node that you want to get the neighbors for.
+		* @return a list with neighbor nodes Indexes.*/
+		virtual std::vector<int> GetNeighbors(int nodeIndex) = 0;
+	};
 
-/** This is the Main class that implemnts the generic A * (A star) search algorithm.
-* How to use it:
-* // ------------------
-* std::shared_ptr<NavMesh> navmesh = std::make_shared<NavMesh>();
-*
-* FindPathEngine engine(navmesh);
-*
-* std::shared_ptr<FindPathEngine::Ticket> ticket = std::make_shared<FindPathEngine::Ticket>(NavMesh::GetIndex(1, 1), NavMesh::GetIndex(6, 6));
-*
-* engine.AddTicket(ticket);
-* 
-* while (!engine.Update())
-* {
-*   /// this loop will stop when the path was found, 
-*   /// or when the path cannot be determined.
-* }
-* // ------------------*/
-class FindPathEngine
-{
-	//forward declaration
+	/** Forward declaration. See bellow the real class.*/
+	class Ticket;
 	class Node;
 
-public:
 
-	/** The constructor.
-	* @param navMesh is a pointer to the user's navmesh class.*/
-	FindPathEngine(std::shared_ptr<NavMeshBase> navMesh);
+	/** This is the Main class that implemnts the generic A * (A star) search algorithm.
+	* How to use it:
+	* // ------------------
+	* std::shared_ptr<NavMesh> navmesh = std::make_shared<NavMesh>();
+	*
+	* FindPathEngine engine(navmesh);
+	*
+	* std::shared_ptr<FindPathEngine::Ticket> ticket = std::make_shared<FindPathEngine::Ticket>(NavMesh::GetIndex(1, 1), NavMesh::GetIndex(6, 6));
+	*
+	* engine.AddTicket(ticket);
+	*
+	* while (!engine.Update())
+	* {
+	*   /// this loop will stop when the path was found,
+	*   /// or when the path cannot be determined.
+	* }
+	* // ------------------*/
+	class FindPathEngine
+	{
+	public:
+
+		/** The constructor.
+		* @param navMesh is a pointer to the user's navmesh class.*/
+		FindPathEngine(std::shared_ptr<NavMeshBase> navMesh);
+
+		/** Add a new request to determine a path */
+		void AddTicket(std::shared_ptr<Ticket> ticket);
+
+		/** This function will run and process every Ticket.
+		* If a ticket is solved (aka the path was found, or there is not solution) just remove the
+		* ticket from the pending list.
+		* @return true if pending list with tickets is empty.*/
+		bool Update();
+
+	private:
+
+		/** Is a pointer to the used's nav mesh. */
+		std::shared_ptr<NavMeshBase> m_navMesh;
+
+
+		/** Update a ticket (aka a search request). This will make another step forward
+		* to search the path.
+		* @param ticket is the request processed
+		* @return true if the job is finished. Return false if the job need to be processed also at the next step.*/
+		bool ProcessTicket(std::shared_ptr<Ticket> ticket);
+
+		/** Is a list with tickets that must be processed. */
+		std::vector<std::shared_ptr<Ticket> > m_tickets;
+	};
+
+
+	/** This is the helper class used internally to
+	* store node's props*/
+	class Node
+	{
+		friend class FindPathEngine;
+	public:
+
+		/** The constructor
+		* @param index is the index of the node.*/
+		Node(int index)
+			: m_index(index)
+			, m_parent(nullptr)
+			, m_cost(-1)
+			, m_distToTarget(-1)
+			, m_f(-1)
+		{
+		}
+
+		~Node()
+		{
+
+		}
+
+	private:
+		/** is the index of the node.*/
+		int m_index;
+
+		/** This is the parent node*/
+		std::shared_ptr<Node> m_parent;
+
+		/** This is the cost to move to this tile. Is called also "G" value. This
+		* must be a sum of parent cost and the cost to move to this node starting from parent.
+		* By default this is -1 which means that this was not calculated yet => the GetCost function
+		* will calculate it.*/
+		int m_cost;//"G"
+
+
+		/** This the distance to target. It is calculated using a heuristic. It is called also the "H" value.
+		* The function ComputeGoalDistanceEstimate will compute it.*/
+		int m_distToTarget;//"H"
+
+		/** This is a sum of "G" and "H" */
+		int m_f; // "G" + "H"
+
+		/** The list with neighbors */
+		std::vector<int> m_neighbors;
+	};
+
 
 	/** This is a ticket used to describe a find path request.*/
 	class Ticket
@@ -98,7 +177,7 @@ public:
 
 		/** Getter for the detected path. */
 		std::vector<int>& GetFoundPath(){ return m_pathFound; }
-		 
+
 	private:
 
 		/** This is the target */
@@ -126,70 +205,6 @@ public:
 		std::map<int, std::shared_ptr<Node> > m_closedList;
 	};
 
-	/** Add a new request to determine a path */
-	void AddTicket(std::shared_ptr<Ticket> ticket);
+} // namespace fpe
 
-	/** This function will run and process every Ticket. 
-	* If a ticket is solved (aka the path was found, or there is not solution) just remove the 
-	* ticket from the pending list.
-	* @return true if pending list with tickets is empty.*/
-	bool Update();
-
-private:
-
-
-	/** This is the helper class used internally to
-	* store node's props*/
-	class Node
-	{
-	public:
-
-		/** The constructor 
-		* @param index is the index of the node.*/
-		Node(int index) 
-			: m_index(index)
-			, m_parent(nullptr)
-			, m_cost(-1)
-			, m_distToTarget(-1)
-			, m_f(-1)
-		{
-		}
-
-		/** is the index of the node.*/
-		int m_index;
-
-		/** This is the parent node*/
-		std::shared_ptr<Node> m_parent;
-
-		/** This is the cost to move to this tile. Is called also "G" value. This
-		* must be a sum of parent cost and the cost to move to this node starting from parent.
-		* By default this is -1 which means that this was not calculated yet => the GetCost function
-		* will calculate it.*/
-		int m_cost;//"G"
-
-
-		/** This the distance to target. It is calculated using a heuristic. It is called also the "H" value.
-		* The function ComputeGoalDistanceEstimate will compute it.*/
-		int m_distToTarget;//"H"
-
-		/** This is a sum of "G" and "H" */
-		int m_f; // "G" + "H"
-
-		/** The list with neighbors */
-		std::vector<int> m_neighbors;
-	};
-
-
-	/** Is a pointer to the used's nav mesh. */
-	std::shared_ptr<NavMeshBase> m_navMesh;
-
-	
-	/** Update a ticket (aka a search request). This will make another step forward 
-	* to search the path.
-	* @param ticket is the request processed
-	* @return true if the job is finished. Return false if the job need to be processed also at the next step.*/
-	bool ProcessTicket(std::shared_ptr<Ticket> ticket);
-
-	/** Is a list with tickets that must be processed. */
-	std::vector<std::shared_ptr<Ticket> > m_tickets;
-};
+#endif //FINDPATHENGINE_H
--- src/FindPathEngine.cpp
+++ src/FindPathEngine.cpp
@@ -1,194 +1,197 @@
 
 #include "FindPathEngine/FindPathEngine.h"
 
-
-FindPathEngine::FindPathEngine(std::shared_ptr<NavMeshBase> navMesh)
-	: m_navMesh(navMesh)
+namespace fpe
 {
-}
-
-FindPathEngine::Ticket::Ticket(int startIndex, int goalIndex)
-	: m_startIndex(startIndex)
-	, m_goalIndex(goalIndex)
-	, m_current(nullptr)
-	, m_state(State::WAITING)
-	, m_steps(0)
-{
-}
-
-/** Add a new request to determine a path */
-void FindPathEngine::AddTicket(std::shared_ptr<Ticket> ticket)
-{
-	m_tickets.push_back(ticket);
-}
-
-/** This function will run and process every Ticket.
-* If a ticket is solved (aka the path was found, or there is not solution) just remove the
-* ticket from the pending list.
-* @return true if pending list with tickets is empty.*/
-bool FindPathEngine::Update()
-{
-	for (auto it = m_tickets.begin(); it != m_tickets.end();)
+	FindPathEngine::FindPathEngine(std::shared_ptr<NavMeshBase> navMesh)
+		: m_navMesh(navMesh)
 	{
-		if (ProcessTicket((*it)))
-			it = m_tickets.erase(it);
-		else
-			++it;
 	}
 
-	return (m_tickets.size() == 0);
-}
-
-bool FindPathEngine::ProcessTicket(std::shared_ptr<Ticket> ticket)
-{
-	ticket->m_steps++;
-
-	/// Chekc if the m_start is the same with m_goalIndex
-	if (ticket->m_startIndex == ticket->m_goalIndex)
+	Ticket::Ticket(int startIndex, int goalIndex)
+		: m_startIndex(startIndex)
+		, m_goalIndex(goalIndex)
+		, m_current(nullptr)
+		, m_state(State::WAITING)
+		, m_steps(0)
 	{
-		ticket->m_pathFound.push_back(ticket->m_startIndex);
-
-		/// Search is stopped because the goal si the same with start node
-		ticket->m_state = Ticket::State::COMPLETED;
-		return true;
 	}
 
-	/// This is the first step -> closed list is empty.
-	/// Add the start node to the closed list.
-	if (ticket->m_closedList.size() == 0)
+	/** Add a new request to determine a path */
+	void FindPathEngine::AddTicket(std::shared_ptr<Ticket> ticket)
 	{
-		std::shared_ptr<Node> start = std::make_shared<Node>(ticket->m_startIndex);
-
-		/// In this case, we will add the start node.
-		start->m_parent = nullptr;
-
-		/// calculate the distance to target
-		start->m_distToTarget = m_navMesh->ComputeGoalDistanceEstimate(ticket->m_goalIndex, ticket->m_startIndex);
-
-		/// calculate the cost to travel from m_startIndex node to the neighbor note
-		start->m_cost = 0;
-
-		/// Calculate the "F" value
-		start->m_f = start->m_distToTarget;
-
-		/// Add the neighbor node to the open list
-		ticket->m_closedList[ticket->m_startIndex] = start;
-
-		/// Set the current node to be the start node
-		ticket->m_current = start;
+		m_tickets.push_back(ticket);
 	}
 
-
-	/// If the start node doe not have valid neighbors, try to GetNeighbors and add them to the open list
-	if (ticket->m_current->m_neighbors.size() == 0)
+	/** This function will run and process every Ticket.
+	* If a ticket is solved (aka the path was found, or there is not solution) just remove the
+	* ticket from the pending list.
+	* @return true if pending list with tickets is empty.*/
+	bool FindPathEngine::Update()
 	{
-		ticket->m_current->m_neighbors = m_navMesh->GetNeighbors(ticket->m_current->m_index);// ticket->m_current->GetNeighbors();
-	}
-
-	for (auto& neighbor : ticket->m_current->m_neighbors)
-	{
-		/// Check if the goal is one of the neighbors
-		if (ticket->m_goalIndex == neighbor)
+		for (auto it = m_tickets.begin(); it != m_tickets.end();)
 		{
-			ticket->m_pathFound.push_back(ticket->m_goalIndex);
+			if (ProcessTicket((*it)))
+				it = m_tickets.erase(it);
+			else
+				++it;
+		}
 
+		return (m_tickets.size() == 0);
+	}
+
+	bool FindPathEngine::ProcessTicket(std::shared_ptr<Ticket> ticket)
+	{
+		ticket->m_state = Ticket::State::PROCESSING;
+
+		ticket->m_steps++;
+
+		/// Chekc if the m_start is the same with m_goalIndex
+		if (ticket->m_startIndex == ticket->m_goalIndex)
+		{
+			ticket->m_pathFound.push_back(ticket->m_startIndex);
+
+			/// Search is stopped because the goal si the same with start node
+			ticket->m_state = Ticket::State::COMPLETED;
+			return true;
+		}
+
+		/// This is the first step -> closed list is empty.
+		/// Add the start node to the closed list.
+		if (ticket->m_closedList.size() == 0)
+		{
+			std::shared_ptr<Node> start = std::make_shared<Node>(ticket->m_startIndex);
+
+			/// In this case, we will add the start node.
+			start->m_parent = nullptr;
+
+			/// calculate the distance to target
+			start->m_distToTarget = m_navMesh->ComputeGoalDistanceEstimate(ticket->m_goalIndex, ticket->m_startIndex);
+
+			/// calculate the cost to travel from m_startIndex node to the neighbor note
+			start->m_cost = 0;
+
+			/// Calculate the "F" value
+			start->m_f = start->m_distToTarget;
+
+			/// Add the neighbor node to the open list
+			ticket->m_closedList[ticket->m_startIndex] = start;
+
+			/// Set the current node to be the start node
+			ticket->m_current = start;
+		}
+
+
+		/// If the start node doe not have valid neighbors, try to GetNeighbors and add them to the open list
+		if (ticket->m_current->m_neighbors.size() == 0)
+		{
+			ticket->m_current->m_neighbors = m_navMesh->GetNeighbors(ticket->m_current->m_index);// ticket->m_current->GetNeighbors();
+		}
+
+		for (auto& neighbor : ticket->m_current->m_neighbors)
+		{
+			/// Check if the goal is one of the neighbors
+			if (ticket->m_goalIndex == neighbor)
+			{
+				ticket->m_pathFound.push_back(ticket->m_goalIndex);
+
+				std::shared_ptr<Node> node = ticket->m_current;
+				do
+				{
+					ticket->m_pathFound.push_back(node->m_index);
+					//std::cout << "result " << node << " " << node->m_index << " " << node->m_f << std::endl;
+					node = node->m_parent;
+				} while (node != nullptr);
+
+				ticket->m_state = Ticket::State::COMPLETED;
+				return true;
+			}
+
+			bool addedAlready = false;
+			std::shared_ptr<Node> neigh = nullptr;
+
+			/// check if is in open list
+			if (ticket->m_openList.find(neighbor) != ticket->m_openList.end())
+			{
+				addedAlready = true;
+				neigh = ticket->m_openList[neighbor];
+			}
+			else if (ticket->m_closedList.find(neighbor) != ticket->m_closedList.end())
+			{
+				addedAlready = true;
+				neigh = ticket->m_closedList[neighbor];
+			}
+
+			if (!addedAlready)
+			{
+				neigh = std::make_shared<Node>(neighbor);
+				neigh->m_parent = ticket->m_current;
+			}
+
+
+
+			/// calculate the distance to target
+			neigh->m_distToTarget = m_navMesh->ComputeGoalDistanceEstimate(ticket->m_goalIndex, neighbor);
+
+			/// calculate the cost to travel from m_startIndex node to the neighbor note
+			neigh->m_cost = m_navMesh->ComputeCost(ticket->m_current->m_index, neighbor);
+
+			/// Calculate the "F" value
+			int f = neigh->m_distToTarget + neigh->m_cost;
+
+			/// If the "F" have the default value it means that this node does not have added 
+			/// to the open list before.
+			if (!addedAlready)
+			{
+				neigh->m_f = f;
+
+				/// Add the neighbor node to the open list
+				ticket->m_openList[neighbor] = neigh;
+			}
+			else if (neigh->m_f > f)
+			{
+				neigh->m_f = f;
+			}
+		}
+
+
+		/// Chekc if there are some nodes in Open list
+		if (ticket->m_openList.size() == 0)
+		{
+			//ticket->m_pathFound.push_back(ticket->m_goalIndex);
 			std::shared_ptr<Node> node = ticket->m_current;
 			do
 			{
 				ticket->m_pathFound.push_back(node->m_index);
 				//std::cout << "result " << node << " " << node->m_index << " " << node->m_f << std::endl;
 				node = node->m_parent;
-			} 
-			while (node != nullptr);
+			} while (node != nullptr);
 
-			ticket->m_state = Ticket::State::COMPLETED;
+			/// If no nodes -> search is stopped due to no path to goal.
+			ticket->m_state = Ticket::State::STOPPED;
 			return true;
 		}
 
-		bool addedAlready = false;
-		std::shared_ptr<Node> neigh = nullptr;
-
-		/// check if is in open list
-		if (ticket->m_openList.find(neighbor) != ticket->m_openList.end())
+		/// Get the object with the 
+		int f = INT_MAX;
+		for (auto& on : ticket->m_openList)
 		{
-			addedAlready = true;
-			neigh = ticket->m_openList[neighbor];
-		}
-		else if (ticket->m_closedList.find(neighbor) != ticket->m_closedList.end())
-		{
-			addedAlready = true;
-			neigh = ticket->m_closedList[neighbor];
+			if (on.second->m_f < f)
+				ticket->m_current = on.second;
 		}
 
-		if (!addedAlready)
-		{
-			neigh = std::make_shared<Node>(neighbor);
-			neigh->m_parent = ticket->m_current;
-		}
+		ticket->m_openList.erase(ticket->m_current->m_index);
+
+		ticket->m_closedList[ticket->m_current->m_index] = ticket->m_current;
+
+		//int i = 0;
+		//for (auto& on : ticket->m_openList)
+		//{
+		//	std::cout << i << " " << on.second << " " << on.first << " " << on.second->m_f << std::endl;
+		//	i++;
+		//}
 
 
-
-		/// calculate the distance to target
-		neigh->m_distToTarget = m_navMesh->ComputeGoalDistanceEstimate(ticket->m_goalIndex, neighbor);
-
-		/// calculate the cost to travel from m_startIndex node to the neighbor note
-		neigh->m_cost = m_navMesh->ComputeCost(ticket->m_current->m_index, neighbor);
-
-		/// Calculate the "F" value
-		int f = neigh->m_distToTarget + neigh->m_cost;
-
-		/// If the "F" have the default value it means that this node does not have added 
-		/// to the open list before.
-		if (!addedAlready)
-		{
-			neigh->m_f = f;
-
-			/// Add the neighbor node to the open list
-			ticket->m_openList[neighbor] = neigh;
-		}
-		else if (neigh->m_f > f)
-		{
-			neigh->m_f = f;
-		}
+		return false;
 	}
-
-
-	/// Chekc if there are some nodes in Open list
-	if (ticket->m_openList.size() == 0)
-	{
-		//ticket->m_pathFound.push_back(ticket->m_goalIndex);
-		std::shared_ptr<Node> node = ticket->m_current;
-		do
-		{
-			ticket->m_pathFound.push_back(node->m_index);
-			//std::cout << "result " << node << " " << node->m_index << " " << node->m_f << std::endl;
-			node = node->m_parent;
-		} while (node != nullptr);
-
-		/// If no nodes -> search is stopped due to no path to goal.
-		ticket->m_state = Ticket::State::STOPPED;
-		return true;
-	}
-
-	/// Get the object with the 
-	int f = INT_MAX;
-	for (auto& on : ticket->m_openList)
-	{
-		if (on.second->m_f < f)
-			ticket->m_current = on.second;
-	}
-
-	ticket->m_openList.erase(ticket->m_current->m_index);
-
-	ticket->m_closedList[ticket->m_current->m_index] = ticket->m_current;
-
-	//int i = 0;
-	//for (auto& on : ticket->m_openList)
-	//{
-	//	std::cout << i << " " << on.second << " " << on.first << " " << on.second->m_f << std::endl;
-	//	i++;
-	//}
-
-
-	return false;
-}
+} //namespace fpe
